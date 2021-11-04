b91d9583d6807252365d341c4c716e2ea2206a19
성하천 <shc0073@gmail.com>
2021-11-04 19:51:24 +0900

    2021년 11월 04일 8 시

--- ShootingGame/ObjectManager.cpp
+++ ShootingGame/ObjectManager.cpp
@@ -35,7 +35,27 @@ void ObjectManager::checkCollision()
 
 				if (colI != nullptr && colJ != nullptr)
 				{
-					cout << "(" << objI->getTag() << " , " << objJ->getTag() << ") ";
+					//충돌 박스 좌표 구하기//
+					float x, y, width, height;
+					float x0I, y0I, x1I, y1I;    //objI(colI)의 박스 충돌체 좌표
+					float x0J, y0J, x1J, y1J;    //objJ(colJ)의 박스 충돌체 좌표
+
+					colI->getBoudingBox(x, y, width, height);
+					x0I = x;
+					y0I = y;
+					x1I = x0I + width;
+					y1I = y0I + height;
+
+					colJ->getBoudingBox(x, y, width, height);
+					x0J = x;
+					y0J = y;
+					x1J = x0J + width;
+					y1J = y0J + height;
+
+					if (y0I < y1J && y1I > y0J && x1I > x0J && x0I < x1J)
+					{
+						cout << "(" << objI->getTag() << " , " << objJ->getTag() << ") 충돌함";
+					}
 				}
 			}
 		}
